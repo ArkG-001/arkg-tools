52983f7b67ec318f4f80a9df37f7acb42f97d242
gehongliang <gehongliang@smartsh.com>
2024-01-24 10:07:26 +0800

    feat: 同步dist

---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+declare namespace arkgTools {
+    /**
+     * 生成数字范围内的随机数
+     * @param min 最小数字
+     * @param max 最大数字
+     * @returns number类型
+     */
+    export function random(min: number, max: number): number
+}
+
+declare module 'arkg-tools' {
+    export = arkgTools
+}
